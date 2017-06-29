41a962a00396821bce40f9d769f7d37a8d46e974
Daniel Patriche <daniel.patriche@mambu.com>
2017-06-29 13:44:18 +0300

    Support encrypted websockets on different Path on same URL/port

--- client/client.js
+++ client/client.js
@@ -37,7 +37,7 @@ function join(channel) {
 	}
 	else {
 		// for local installs
-		ws = new WebSocket('ws://' + document.domain + ':6060')
+		ws = new WebSocket('wss://' + document.domain + '/chat-ws')
 	}
 
 	var wasConnected = false
